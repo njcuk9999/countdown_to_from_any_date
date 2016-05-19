1028e6b9d0b2597014ea29ee9bbe77d70ff3330b
Neil Cook <neil.james.cook@gmail.com>
2016-05-19 12:14:44 +0100

    Update countdown.py

--- countdown.py
+++ countdown.py
@@ -4,22 +4,22 @@ from calendar import timegm
 import time
 import os
 import sys
-
+import Tkinter as tk
 
 # ==============================================================================
 # Define variables
 # ==============================================================================
 # define starting date
-startyear = 2014
-startmonth = 10
-startday = 2
+startyear = 2015
+startmonth = 03
+startday = 01
 starthour = 12
 startminute = 0
 startsecond = 0
 # define end date
-endyear = 2017
-endmonth = 3
-endday = 31
+endyear = 2018
+endmonth = 9
+endday = 01
 endhour = 12
 endminute = 0
 endsecond = 0
@@ -28,13 +28,15 @@ endsecond = 0
 #           - "percentage"
 #           - "count down"
 #           - "count since"
-countdowntype = "count since"
-
+countdowntype = "count down"
 # define the number of decimal places for percent
 percent_decimals = 8
 # define whether we want the count down in years, months, days, hours,
 # minutes or seconds
 countdown_units = "days"
+# use gui
+gui = True
+
 
 # ==============================================================================
 # Define functions
@@ -125,8 +127,9 @@ def unix_time(dttm=None):
 def count(startlist=None, endlist=None, kind="percentage",
           pdec=8, cunit="days"):
     """
-    Displays a percentage of a way between two dates, or count down to a date, or a count since a certain date
-    
+    Displays a percentage of a way between two dates, or count down to a date,
+    or a count since a certain date
+
     :param startlist: list of integers [year, month, day, hour, minute, second]
                       defining the start of the count down
 
@@ -147,9 +150,6 @@ def count(startlist=None, endlist=None, kind="percentage",
                     - "hours"
                     - "minutes"
                     - "seconds"
-                    - "milliseconds"
-                    - "microseconds"
-                    - "nanoseconds"
 
     :return:
     """
@@ -165,10 +165,10 @@ def count(startlist=None, endlist=None, kind="percentage",
 
     convert = dict(years=365.25 * 24 * 3600.0,
                    months=365.25 * 24 * 3600 / 12.0,
-                   days=24 * 3600.0, hours=3600.0,
-                   minutes=60.0, seconds=1.0,
-                   milliseconds=1e-3, microseconds=1e-6,
-                   nanoseconds=1e-9)
+                   days=24 * 3600.0,
+                   hours=3600.0,
+                   minutes=60.0,
+                   seconds=1.0)
 
     fmt = '{0:02.0f}-{1:02.0f}-{2:02.0f} '
     fmt += '{3:02.0f}:{4:02.0f}:{5:02.2f}'
@@ -210,6 +210,98 @@ def count(startlist=None, endlist=None, kind="percentage",
             time.sleep(0.1)
 
 
+class App(tk.Frame):
+    def __init__(self, master=None):
+        # start display
+        tk.Frame.__init__(self, master)
+        self.parent = master
+
+        self.width = 640
+        self.height = 360
+
+        self.parent.minsize(width=self.width, height=self.height)
+
+        self.can = tk.Canvas(self.parent, bg='red', height=self.height,
+                             width=self.width)
+        self.can.place(x=0, y=0, anchor=tk.NW)
+
+        self.create_widgets()
+
+
+    def create_widgets(self):
+        self.kind = countdowntype
+        self.cunit = countdown_units
+        self.pdec = percent_decimals
+        startlist, endlist = startl, endl
+        # deal with start and end
+        if startlist is None:
+            self.start = unix_time()
+        else:
+            self.start = unix_time(datetime(*startlist))
+        if endlist is None:
+            self.end = unix_time()
+        else:
+            self.end = unix_time(datetime(*endlist))
+
+        self.convert = dict(years=365.25 * 24 * 3600.0,
+                            months=365.25 * 24 * 3600 / 12.0,
+                            days=24 * 3600.0,
+                            hours=3600.0,
+                            minutes=60.0,
+                            seconds=1.0)
+        fmt = '{0:02.0f}-{1:02.0f}-{2:02.0f} '
+        fmt += '{3:02.0f}:{4:02.0f}:{5:02.2f}'
+
+        # sort out the title
+        if self.kind in ["percentage", "pc", r"%"]:
+            title = '\tPercentage From ' + fmt.format(*startlist)
+            title += ' To ' + fmt.format(*endlist)
+        elif self.kind == "count down":
+            title = '\tCount down to ' + fmt.format(*endlist)
+        else:
+            title = '\tTime since ' + fmt.format(*startlist)
+
+        self.parent.title(title)
+
+        self.QUIT = tk.Button(self.can, text="QUIT", fg="red",
+                              command=root.destroy)
+        self.QUIT.place(x=self.width/2.0, y=self.height, anchor=tk.S)
+
+        self.msg = tk.StringVar()
+        self.clock = tk.Label(self.can, font=('Helvetica', 24))
+        # self.clock.pack(side="top")
+        self.clock.place(x=self.width/2.0, y=self.height/2.0,
+                         anchor=tk.CENTER)
+        self.clock["textvariable"] = self.msg
+
+        # now call update clock
+        self.update_clock()
+
+
+
+    def update_clock(self):
+        if self.kind in ["percentage", "pc", r"%"]:
+            tick1 = unix_time()
+            # display percentage
+            num = float(tick1 - self.start)
+            denom = float(self.end - self.start)
+            percent = (num/denom) * 100.0
+            msg = r'{0:.8f} %'.format(percent)
+        elif self.kind == "count down":
+            units = self.convert[self.cunit]
+            self.start = unix_time()
+            diff = self.end - self.start
+            msg = '\t{0:,.8f} {1}'.format(diff / units, self.cunit)
+        else:
+            units = self.convert[self.cunit]
+            self.end = unix_time()
+            diff = self.end - self.start
+            msg = '\t{0:,.8f} {1}'.format(diff / units, self.cunit)
+        self.msg.set(msg)
+
+        self.after(10, self.update_clock)
+
+
 # ==============================================================================
 # Start of code
 # ==============================================================================
@@ -218,20 +310,21 @@ if __name__ == '__main__':
     # set up starting parameters as list of integers
     # [year, month, day, hour, minute, second] defining the start of
     # the count down
-    start = [startyear, startmonth, startday,
-             starthour, startminute, startsecond]
+    startl = [startyear, startmonth, startday,
+              starthour, startminute, startsecond]
     # set up ending parameters as list of integers
     # [year, month, day, hour, minute, second] defining the start of
     # the count down
-    end = [endyear, endmonth, endday, endhour, endminute, endsecond]
+    endl = [endyear, endmonth, endday, endhour, endminute, endsecond]
 
     # run the count
-    count(start, end, countdowntype, percent_decimals, countdown_units)
+    if not gui:
+        count(startl, endl, countdowntype, percent_decimals, countdown_units)
+    else:
+        root = tk.Tk()
+        app = App(master=root)
+        root.mainloop()
+
 # =============================================================================
 # End of code
 # =============================================================================
-
-
-
-
-
